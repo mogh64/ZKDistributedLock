156a20a91948a33fca548d92c8d09b6929dad0ec
Mortaza Ghahremani <mortaza.ghahremani@gmail.com>
2022-01-23 16:30:07 +0330

    add overload to locker

--- LockManagement/DistributedLock/IDistributedLock.cs
+++ LockManagement/DistributedLock/IDistributedLock.cs
@@ -4,5 +4,8 @@
     public interface IDistributedLock
     {
         Task LockAsync(Action lockAcquiredAction, Action? lockReleasedAction = null);
+        Task LockAsync<T>(Action<T> lockAcquiredAction, T input, Action? lockReleasedAction = null);
+        Task LockAsync<T1, T2>(Action<T1, T2> lockAcquiredAction, T1 input1, T2 input2, Action? lockReleasedAction = null);
+        Task LockAsync<T1, T2,T3>(Action<T1, T2,T3> lockAcquiredAction, T1 input1, T2 input2,T3 input3, Action? lockReleasedAction = null);
     }
 }
