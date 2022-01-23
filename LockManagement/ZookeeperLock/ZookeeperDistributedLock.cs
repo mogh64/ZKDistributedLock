0f7e26496ecf689727fa617a6e7cffaa49380881
Mortaza Ghahremani <mortaza.ghahremani@gmail.com>
2022-01-23 16:31:11 +0330

    add lock overloads

--- LockManagement/ZookeeperLock/ZookeeperDistributedLock.cs
+++ LockManagement/ZookeeperLock/ZookeeperDistributedLock.cs
@@ -33,5 +33,59 @@ namespace LockManagement.ZookeeperLock
 
             return writeLock.Lock();
         }
+         public Task LockAsync<T>(Action<T> lockAcquiredAction, T input1, Action? lockReleasedAction = null)
+        {
+            var lockCallback = new LockCallback(() =>
+            {
+                try
+                {
+                    lockAcquiredAction(input1);
+                }
+                finally
+                {
+                    writeLock.unlock();
+                }
+            }, lockReleasedAction);
+
+            writeLock.setLockListener(lockCallback);
+
+            return writeLock.Lock();
+        }
+        public Task LockAsync<T1, T2>(Action<T1, T2> lockAcquiredAction, T1 input1, T2 input2, Action? lockReleasedAction = null)
+        {
+            var lockCallback = new LockCallback(() =>
+            {
+                try
+                {
+                    lockAcquiredAction(input1, input2);
+                }
+                finally
+                {
+                    writeLock.unlock();
+                }
+            }, lockReleasedAction);
+
+            writeLock.setLockListener(lockCallback);
+
+            return writeLock.Lock();
+        }
+        public Task LockAsync<T1, T2,T3>(Action<T1, T2,T3> lockAcquiredAction, T1 input1, T2 input2,T3 input3, Action? lockReleasedAction = null)
+        {
+            var lockCallback = new LockCallback(() =>
+            {
+                try
+                {
+                    lockAcquiredAction(input1, input2,input3);
+                }
+                finally
+                {
+                    writeLock.unlock();
+                }
+            }, lockReleasedAction);
+
+            writeLock.setLockListener(lockCallback);
+
+            return writeLock.Lock();
+        }
     }
 }
